e6121d39cdb40b375d317582d3b3bd65cb4bd97e
ThakeerMoola <st10213212@rcconnect.edu.za>
2023-04-25 14:09:13 +0200

    UPDATED
    Add project files.
    
    Add .gitattributes, .gitignore, and README.md.

---
+++ PoePart1/Program.cs
@@ -0,0 +1,103 @@
+﻿
+
+using PoePart1;
+using System;
+
+namespace RecipeApplication
+{
+    class Program
+    {
+        //main method called
+            static void Main(string[] args)
+            {
+            //an instsance of the recipe class is created 
+                Recipe recipe = new Recipe(); // create an instance of Recipe class
+                recipe.recipeDisplay(); // call recipeDisplay method
+
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+//// to call methods from a class eg class recipe, 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////        Console.WriteLine("Recipe added successfully!");
+////        Console.WriteLine("Recipe name: " + name);
+////        Console.WriteLine("Ingredients:");
+
+////        for (int i = 0; i < amount; i++)
+////        {
+////            Console.WriteLine("- " + ingredients[i]);
+////        }
+
+////        Console.ReadLine();
+
+
+////        Console.WriteLine("the number of steps involved");
+////        int steps = int.Parse(Console.ReadLine());
+
+////        String[] step = new String[steps];
+////        for (int s = 0; s < steps; s++)
+////        {
+////            Console.WriteLine("step " + (s + 1) + "with a description"+ " :");
+////            step[s] = Console.ReadLine();
+////        }
+////        Console.WriteLine("steps added successfully! " );
+
+////        for (int s = 0; s < steps; s++)
+////        {
+////            Console.WriteLine("- " + step[s]);
+////        }
+
+////        Console.ReadLine();
+
+
+////    }
+////}
+
+
+
+////        Console.WriteLine("for each of the steps, provide us with a description of what the user should do");
+////        String description = Console.ReadLine();
+
+////        Console.WriteLine("the number of ingredients used for this special dish are as follows\n" +
+////            "--------------------------------------------------------\n");
+////        Console.WriteLine("number of ingredients : " +amount);
+////        Console.WriteLine("ingredients used for this dish : " +name);
+
+////        Console.WriteLine("units of measurements for the ingrediant : " + units);
+////        Console.WriteLine("steps involved :" + steps);
+////        Console.WriteLine("description on how to make the special dish : " + description);
+
+////    }
+////}
+
+//// call im main class from child class
+////recipe obj = new recipe();
+////obj.display();
+////obj.scalling;
---
+++ PoePart1/Recipe.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoePart1
+{
+    internal class Recipe
+    {
+        //class repicedisplay created
+        public void recipeDisplay()
+        {
+
+            Console.WriteLine("---------------------------------------------------" +
+                "\nWelcome to Recipe World, your house to tasty foods!\n" +
+                "---------------------------------------------------\n");
+
+            Console.WriteLine("\nPlease enter the number of ingredients for your special dish: ");
+
+
+            //parsing an integer through the readline statement, user only aloowed to enter a number, or else error!
+            int amount = int.Parse(Console.ReadLine());
+
+        
+       
+
+            //array to save ingredients and loop the amount of ingredients needed
+            string[] ingredients = new string[amount];
+            string[] units = new string[amount];
+            double[] quantity = new double[amount];
+
+            //loop fo the information entered by user 
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine($"\nEnter ingredient {i + 1} name:");
+                ingredients[i] = Console.ReadLine();
+
+                Console.WriteLine($"\nEnter quantity for {ingredients[i]}:");
+                quantity[i] = double.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\nEnter units of measurement for {ingredients[i]}:");
+                units[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nPlease enter the number of steps involved in this recipe:");
+            int numsteps = int.Parse(Console.ReadLine());
+
+            string[] steps = new string[numsteps];
+
+            for (int i = 0; i < numsteps; i++)
+            {
+                Console.WriteLine($"\nEnter step {i + 1} and a description:");
+                steps[i] = Console.ReadLine();
+            }
+
+
+            Console.WriteLine("--------------------------------\n" +
+                "Ingredients:\n" +
+                "--------------------------------\n");
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.WriteLine($" {ingredients[i]} - {quantity[i]} {units[i]}\n");
+
+            }
+
+            Console.WriteLine("--------------------------------\n" +
+                "Instructions:\n" +
+                "--------------------------------\n");
+
+            for (int i = 0; i < numsteps; i++)
+            {
+                Console.WriteLine($" Step {i + 1}. {steps[i]}\n");
+            }
+
+
+            Console.WriteLine("Enter scaling factor (0.5, 2, or 3)\n" +
+                " '4' to reset quantities\n '5' to clear recipe:");
+
+            string input = Console.ReadLine();
+
+            if (input == "4")
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    ingredients[i] = default;
+                    quantity[i] = default;
+                    units[i] = default;
+                }
+                for (int i = 0; i < numsteps; i++)
+                {
+                    steps[i] = default;
+
+                    Console.WriteLine($" {ingredients[i]} - {quantity[i]} {units[i]}\n");
+                    
+                }
+                recipeDisplay();
+
+            }
+            else if (input == "5")
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    quantity[i] /= 2;
+
+                    Console.WriteLine($" {ingredients[i]} - {quantity[i]} {units[i]}\n");
+                   
+                }
+                recipeDisplay();
+
+            }
+            else
+            {
+                double scalingfactor = double.Parse(input);
+                for (int i = 0; i < amount; i++)
+                {
+                    quantity[i] *= scalingfactor;
+
+                    Console.WriteLine($" {ingredients[i]} - {quantity[i]}{units[i]}\n");
+                  
+                }
+                recipeDisplay();
+            }
+        }
+    }
+}
+
