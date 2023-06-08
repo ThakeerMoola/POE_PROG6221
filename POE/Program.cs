7e1574fe40e78dd413a892892a9e45025d506615
ThakeerMoola <st10213212@rcconnect.edu.za>
2023-06-08 08:45:25 +0200

    removed

--- POE/Program.cs
+++ POE/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+    }
+}
